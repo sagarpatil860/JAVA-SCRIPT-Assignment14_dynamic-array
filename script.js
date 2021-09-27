e8719ac93a4e2c59a635d977130b60c13e88e6e4
DESKTOP-H0R2DF3\Sagarpatil <sagarpatil860@gmail.com>
2021-09-28 02:07:26 +0530

    para dom api added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("script connection succesful");
 let x=new Array;
 let clickcount=0;
+let str=new String;
+str="";
 
 x=[];
 
@@ -8,7 +10,6 @@ document.getElementById("btnadd").addEventListener("click",funarray);
 function funarray(){
     ++clickcount;
     console.log("click count is:"+clickcount);
-
     console.log("inside array cration")
     let z =parseInt(document.getElementById("tx").value)
     console.log("value passed in text field is :"+z);
@@ -16,7 +17,16 @@ function funarray(){
     x.push(addobj(z));
     for(let i=0; i<clickcount; i++){
     console.log(i+" index object name is"+x[i].name           +",game is :"+x[i].game);
-    }
+    
+}
+
+// adding new paragraphs to display data collection
+var pp = document.createElement("P");
+var t = document.createTextNode("arrayindex:"+(clickcount-1) +"object content name;"+x[clickcount-1].name);
+pp.appendChild(t);
+document.body.appendChild(pp);
+
+
     
     console.log("script end")
 
@@ -38,5 +48,7 @@ document.getElementById("btnft").addEventListener("click",fetchdata);
 function fetchdata(){
     let fetchindex=parseInt(document.getElementById("ft").value);
     document.getElementById("cont").innerHTML=x[fetchindex].name;
+    // document.getElementById("arraycontent").innerHTML=;
+        
 }
 
